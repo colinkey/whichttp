eae4775384f5bf4576e528c2306264b867f03577
Colin Key <colin.m.key@gmail.com>
2021-12-21 21:04:14 -0700

    Update method loader to be more resilient to files

--- src/method_loader.cr
+++ src/method_loader.cr
@@ -2,8 +2,18 @@ require "yaml"
 
 class MethodLoader
   def self.load
-    yaml = File.open("./src/http_status_codes.yml") do |file|
+    yaml = File.open(resolve_file_name) do |file|
       YAML.parse(file)
     end
+  rescue e
+    puts e
+    Process.exit()
+  end
+
+  def self.resolve_file_name
+    return "./http_status_codes.yml" if File.exists?("./http_status_codes.yml")
+    return "./src/http_status_codes.yml" if File.exists?("./src/http_status_codes.yml")
+
+    raise "Unable to resolve status codes config"
   end
 end
