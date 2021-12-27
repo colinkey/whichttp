8b58869966ef9d134f2139bea926e430a98d7b0e
Colin Key <colin.m.key@gmail.com>
2021-12-26 20:11:47 -0700

    Adds help and acknowledge flags for cli

--- src/cli.cr
+++ src/cli.cr
@@ -9,15 +9,26 @@ class Cli
   end
 
   def self.call
-    code = nil
-    symbol = nil
+    passed_code = nil
+    passed_symbol = nil
+
     OptionParser.parse do |parser|
-      parser.on "-c PASSED_CODE", "--code PASSED_CODE", "Look up status by code. Default behavior of Whichttp and flag is optional" do |passed_code|
-        code = passed_code
+      parser.on "-c CODE", "--code CODE", "Look up status by code. Default behavior of Whichttp and flag is optional" do |code|
+        passed_code = code
       end
 
-      parser.on "-s PASSED_SYMBOL", "--symbol PASSED_SYMBOL", "Look up status by rails symbol" do |passed_symbol|
-        symbol = passed_symbol
+      parser.on "-s SYMBOL", "--symbol SYMBOL", "Look up status by rails symbol" do |symbol|
+        passed_symbol = symbol
+      end
+
+      parser.on "-h", "--help", "Show help" do
+        puts parser
+        exit
+      end
+
+      parser.on "-a", "--acknowledgements", "Show acknowledgments" do
+        puts acknowledgements
+        exit
       end
 
       parser.unknown_args do |args|
@@ -26,9 +37,15 @@ class Cli
     end
 
     CliOptions.new(
-      code: code,
-      symbol: symbol,
+      code: passed_code,
+      symbol: passed_symbol,
     )
+  end
+
+  def self.acknowledgements
+    "Data for the HTTP status codes was obtained from the wikipedia entry for HTTP status codes at https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+Data for the valid rails symbols was obtained from the following gist https://gist.github.com/mlanett/a31c340b132ddefa9cca
+Data is as accurate as of the last update on December 20th, 2021"
   end
 end
 
